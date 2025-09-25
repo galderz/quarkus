0c7e4e9bde40bd1c8dc7c79af8cc6c54e64b4d48
Galder Zamarreño <galder@ibm.com>
2025-09-25 11:26:18 +0200

    Avoid blanket initialization of io.netty package as build time init

--- extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyOverrideMetadata.java
+++ extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyOverrideMetadata.java
@@ -10,6 +10,10 @@ public class NettyOverrideMetadata {
     static final String NETTY_CODEC_REFLECT_CONFIG_MATCH_REGEX = "/META-INF/native-image/io\\.netty/netty-codec/generated/handlers/reflect-config\\.json";
     static final String NETTY_HANDLER_JAR_MATCH_REGEX = "io\\.netty\\.netty-handler";
     static final String NETTY_HANDLER_REFLECT_CONFIG_MATCH_REGEX = "/META-INF/native-image/io\\.netty/netty-handler/generated/handlers/reflect-config\\.json";
+    static final String NETTY_CODEC_HTTP_JAR_MATCH_REGEX = "io\\.netty\\.netty-codec-http";
+    static final String NETTY_CODEC_HTTP_RESOURCE_MATCH_REGEX = "/META-INF/native-image/io\\.netty/netty-codec-http/native-image\\.properties";
+    static final String NETTY_CODEC_HTTP2_JAR_MATCH_REGEX = "io\\.netty\\.netty-codec-http2";
+    static final String NETTY_CODEC_HTTP2_RESOURCE_MATCH_REGEX = "/META-INF/native-image/io\\.netty/netty-codec-http2/native-image\\.properties";
 
     @BuildStep
     void excludeNettyDirectives(BuildProducer<ExcludeConfigBuildItem> nativeImageExclusions) {
@@ -17,5 +21,9 @@ public class NettyOverrideMetadata {
                 .produce(new ExcludeConfigBuildItem(NETTY_CODEC_JAR_MATCH_REGEX, NETTY_CODEC_REFLECT_CONFIG_MATCH_REGEX));
         nativeImageExclusions
                 .produce(new ExcludeConfigBuildItem(NETTY_HANDLER_JAR_MATCH_REGEX, NETTY_HANDLER_REFLECT_CONFIG_MATCH_REGEX));
+        nativeImageExclusions
+                .produce(new ExcludeConfigBuildItem(NETTY_CODEC_HTTP_JAR_MATCH_REGEX, NETTY_CODEC_HTTP_RESOURCE_MATCH_REGEX));
+        nativeImageExclusions
+                .produce(new ExcludeConfigBuildItem(NETTY_CODEC_HTTP2_JAR_MATCH_REGEX, NETTY_CODEC_HTTP2_RESOURCE_MATCH_REGEX));
     }
 }
