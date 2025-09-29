cbfe5a3e2a40b86475b502b87cfcc310255c125a
Galder Zamarreño <galder@ibm.com>
2025-09-29 12:08:30 +0200

    Reflection BaseMpscLinkedArrayQueueProducerFields.producerIndex

--- core/deployment/src/main/java/io/quarkus/deployment/logging/LoggingResourceProcessor.java
+++ core/deployment/src/main/java/io/quarkus/deployment/logging/LoggingResourceProcessor.java
@@ -190,6 +190,7 @@ public final class LoggingResourceProcessor {
                 "io.netty.util.internal.shaded.org.jctools.queues.unpadded.MpscUnpaddedArrayQueueProducerIndexField",
                 "io.netty.util.internal.shaded.org.jctools.queues.unpadded.MpscUnpaddedArrayQueueProducerLimitField",
                 "io.netty.util.internal.shaded.org.jctools.queues.unpadded.MpscUnpaddedArrayQueueConsumerIndexField",
+                "io.netty.util.internal.shaded.org.jctools.queues.BaseMpscLinkedArrayQueueProducerFields",
                 "jakarta.transaction.RollbackException").fields().methods().build();
     }
 
