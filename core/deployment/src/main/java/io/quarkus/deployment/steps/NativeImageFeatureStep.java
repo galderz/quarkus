3af64967ca525c0b2f24d72b68838e6b206e036a
Galder Zamarreño <galder@ibm.com>
2025-10-08 18:01:49 +0200

    Security providers runtime init changes for Pulsar

--- core/deployment/src/main/java/io/quarkus/deployment/steps/NativeImageFeatureStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/steps/NativeImageFeatureStep.java
@@ -98,7 +98,8 @@ public class NativeImageFeatureStep {
         overallCatch.invokeStaticMethod(RUN_TIME_INITIALIZATION,
                 overallCatch.marshalAsArray(String.class, overallCatch.load("io.smallrye.common.classloader.ClassPathUtils"),
                         overallCatch.load("io.quarkus.fs.util.FileSystemProviders"),
-                        overallCatch.load("org.apache.kafka.common.security.ssl"))); // force runtime initialization on specific packages
+                        overallCatch.load("org.apache.kafka.common.security.ssl"),
+                        overallCatch.load("org.apache.pulsar.common.util.SecurityUtility"))); // force runtime initialization on specific packages
 
         // Set the user.language and user.country system properties to the default locale
         // The deprecated option takes precedence for users who are already using it.
